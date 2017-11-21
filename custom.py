6320767fe628ce70f1e1167bf3ff58c1a99737bd
castelhanog <guinnesscoder@gmail.com>
2017-11-20 23:37:50 -0200

    v 0.1

---
+++ custom.py
@@ -0,0 +1,5 @@
+color = "#729796"
+
+font1 = ('Arial', '16', 'bold') # Títulos
+
+font2 = ('Times', '13', 'bold') # Label informativas
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 import shelve
-
-color = "#729796"
+from custom import *
 
 
 class principal(object):
@@ -10,7 +9,7 @@ class principal(object):
         self.a['bg'] = color
         self.dbs = shelve.open('selldb')
 
-        self.l1 = Label(self.a, text = 'Módulo de vendas', bg = color, fg = 'white', font = ('Arial', '16', 'bold'))
+        self.l1 = Label(self.a, text = 'Módulo de vendas', bg = color, fg = 'white', font = font1)
         self.l1.grid(row = 1, column = 2, pady = 3)
 
         self.l2 = Label(self.a, text = 'Cliente', bg = color)
@@ -25,7 +24,7 @@ class principal(object):
         self.e2 = Entry(self.a)
         self.e2.grid(row = 5, column = 2, pady = 3)
 
-        self.l5 = Label(self.a, text = "", bg = color, pady = 3, font = ('Times', '13', 'bold'))
+        self.l5 = Label(self.a, text = "", bg = color, pady = 3, font = font2)
         self.l5['bg'] = color
         self.l5.grid(row = 6, column = 2)
 
