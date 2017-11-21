b3ba1475e5ce81f1046532ba67c0056a59778bd5
casterlhanog <guinnesscoder@gmail.com>
2017-11-21 15:02:39 -0200

    v 0.2

--- main.py
+++ main.py
@@ -10,39 +10,45 @@ class principal(object):
         self.a['bg'] = color
         self.dbs = shelve.open('selldb')
 
+        self.l0 = Radiobutton(self.a, text = 'Módulo Venda', value = 0)
+        self.l0.grid(row = 1, column = 1)
+
+        self.l01 = Radiobutton(self.a, text = 'Módulo Pedido', value = 1)
+        self.l01.grid(row = 1, column = 3)
+
         self.l1 = Label(self.a, text = 'Módulo de vendas', bg = color, fg = 'white', font = font1)
-        self.l1.grid(row = 1, column = 2, pady = 3)
+        self.l1.grid(row = 2, column = 2, pady = 3)
 
         self.l2 = Label(self.a, text = 'Cliente', bg = color, font = font3)
-        self.l2.grid(row = 2, column = 2, pady = 3)
+        self.l2.grid(row = 3, column = 2, pady = 3)
 
         self.e1 = Entry(self.a)
-        self.e1.grid(row = 3, column = 2, pady = 3)
+        self.e1.grid(row = 4, column = 2, pady = 3)
 
         self.l3 = Label(self.a, text = "Valor", bg = color, font = font3)
-        self.l3.grid(row = 4, column = 2, pady = 3)
+        self.l3.grid(row = 5, column = 2, pady = 3)
 
         self.e2 = Entry(self.a)
-        self.e2.grid(row = 5, column = 2, pady = 3)
+        self.e2.grid(row = 6, column = 2, pady = 3)
 
         self.l5 = Label(self.a, text = "", bg = color, font = font2)
         self.l5['bg'] = color
-        self.l5.grid(row = 6, column = 2, pady = 3)
+        self.l5.grid(row = 7, column = 2, pady = 3)
 
         self.b1 = Button(self.a, text ='Registra venda')
         self.b1.bind('<Button-1>', self.venda)
         self.b1.bind('<Return>', self.venda)
-        self.b1.grid(row = 7, column = 1)
+        self.b1.grid(row = 8, column = 1)
 
         self.b2 = Button(self.a, text = 'Abater valor')
         self.b2.bind('<Button-1>', self.abate)
         self.b2.bind('<Return>', self.abate)
-        self.b2.grid(row = 7, column = 2)
+        self.b2.grid(row = 8, column = 2)
 
         self.b3 = Button(self.a, text = 'Ver saldo')
         self.b3.bind('<Button-1>', self.saldo)
         self.b3.bind('<Return>', self.saldo)
-        self.b3.grid(row = 7, column = 3)
+        self.b3.grid(row = 8, column = 3)
 
     def venda(self, event):
         c = self.e1.get()
